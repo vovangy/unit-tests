e76a1a2d6f6b0e2e687e54eb684c5a72f8f08d90
Владимир <v.malenko@automacon.ru>
2024-10-03 19:41:08 +0300

    Revert "init"
    This reverts commit d36b61956a87767bd7caee13e93caf80744819a2.

--- backend/src/test_calculator.py
+++ backend/src/test_calculator.py
@@ -1,5 +1,6 @@
 import unittest
-from calculator import Calculator
+from src.calculator import Calculator
+
 
 class TestCalculator(unittest.TestCase):
 
@@ -8,37 +9,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calculator.addition(1, 2), 3)
-    
-    def test_add_float(self):
-        self.assertEqual(self.calculator.addition(1.2, 2.1), 3.3)
 
-    def test_multiplication(self):
-        self.assertEqual(self.calculator.multiplication(1, 2), 2)
-    
-    def test_multiplication_zero(self):
-        self.assertEqual(self.calculator.multiplication(1, 0), 0)
-
-    def test_multiplication_float(self):
-        self.assertEqual(self.calculator.multiplication(2.4, 3), 2.4 * 3)
-
-def addition_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(TestCalculator('test_add'))
-    suite.addTest(TestCalculator('test_add_float'))
-    return suite
-
-def multiplication_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(TestCalculator('test_multiplication'))
-    suite.addTest(TestCalculator('test_multiplication_zero'))
-    suite.addTest(TestCalculator('test_multiplication_float'))
-    return suite
 
 if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-
-    print("Running addition tests:")
-    runner.run(addition_suite())
-
-    print("\nRunning multiplication tests:")
-    runner.run(multiplication_suite())
+    unittest.main()
